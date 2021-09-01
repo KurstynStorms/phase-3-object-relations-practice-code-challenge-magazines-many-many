6ef8da195fbd9cf0874ff17438ef6c368f15c91c
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-09-01 08:47:16 -0400

    updated

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,16 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through :subscriptions
+
+  def email_list
+    emails = readers.pluck(:email)
+    emails.join(";")
+  end
+
+  def self.most_popular
+    subscribers_with_count = Magazine.all.map do |magazine|
+      magazine.subscriptions.count 
+    end.max
+    Magazine.all.find {|magazine|}
+  end
 end

6e411d05f5e2e78e46f393123256ccbad47d998f
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-09-01 08:45:52 -0400

    gill's livecode

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,20 @@
 class Reader < ActiveRecord::Base
-  
+  has_many: subscriptions
+  has_many: magazines, through: subscriptions
+
+  def subscribe(magazine, price)
+    Subscription.create(
+      magazine: magazine.
+      reader_id: self.id,
+      price: price
+    )
+  end
+
+  def total_subscription_price
+    subscription.sum(:price)
+  end
+
+  def cancel_subscription
+    Subscription.find_by(magazine: magazine, reader: self).destroy
+  end
 end
